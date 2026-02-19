49916f780708326f65b171a334e0053fbd733d62
Ayush Raj <ayussshhraj@gmail.com>
2026-02-19 23:18:21 +0530

    Arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,58 @@
+const user = {
+    username: "ayush",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+
+
+// function chai(){
+//     let username = "ayush"
+//     console.log(this.username);
+// }
+
+// chai()
+
+
+// const chai = function () {
+//     let username = "ayush"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "ayush"
+    // console.log(this);
+}
+
+
+// chai()
+
+
+//      arrow function
+// const addTwo = (num1, num2) => {
+//     return num1 + num2       // explicit return
+// }
+
+//      implicit return
+// const addTwo = (num1, num2) =>  num1 + num2
+     
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+
+const addTwo = (num1, num2) => ({username: "ayush"})       // to return object we need to wrap in parenthesis
+
+
+console.log(addTwo(3, 4))
+
+
