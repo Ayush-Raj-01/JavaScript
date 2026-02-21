8a84f688fcb4229980961bbd7dd3e9b66bb7b985
Ayush Raj <ayussshhraj@gmail.com>
2026-02-22 02:34:34 +0530

    Guess the Number

---
+++ 07_projects/04-GuessTheNumber/script.js
@@ -0,0 +1,86 @@
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+let randomNumber = parseInt(Math.random() * 100 + 1)
+
+const p = document.createElement(p)
+
+let prevGuess = []
+let numGuess = 1
+
+let playGame = true
+
+
+if(playGame){
+    submit.addEventListener('click' , function(e) {
+        e.preventDefault()
+        const guess = parseInt(userInput.value)
+        validateGuess(guess)
+    })
+}
+
+
+function validateGuess(guess){
+    if(isNaN(guess)){
+        alert ('Please enter a valid number')
+    }
+    else if(guess < 1){
+        alert('PLease enter a number more than 1');
+    }
+    else if(guess > 100){
+        alert('PLease enter a number less than 100');
+    }
+    else{
+        prevGuess.push(guess)
+        if(numGuess === 10){
+            displayGuess(guess)
+            displayMessage(`Game Over. Random number was ${randomNumber}`);
+            endGame()
+        }
+        else{
+            displayGuess(guess)
+            checkGuess(guess)
+        }
+    }
+}
+
+
+function checkGuess(guess){
+    if(guess === randomNumber){
+        displayMessage(`You guessed it right`);
+        endGame()
+    }
+    else if(guess > randomNumber){
+        displayMessage(`Number is TOOO High`);
+    }
+    else{
+        displayMessage(`Number is TOOO Low`);
+    }
+}
+
+// it actually cleans the guess and display past guesses and update remaning guesses
+function displayGuess(guess){
+    userInput = ''
+    guessSlot.innerHTML = `${guess} , `
+    numGuess++;
+    remaining.innerHTML =  `${11 - numGuess}`
+}
+
+
+function displayMessage(message){
+    lowOrHi.innerHTML = `<h2>${message}</h2>`
+}
+
+
+function endgame(){
+
+}
+
+
+function newGame(){
+
+}
